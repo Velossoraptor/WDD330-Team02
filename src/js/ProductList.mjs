eeca902f068697117b04d85e4e884bf6374303f0
velossoraptor <taliamolsen@gmail.com>
2025-09-11 11:26:08 -0400

    Discount Indicator-Product Listing

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,6 +1,18 @@
 import { renderListWithTemplate } from "./utils.mjs";
 
 function productCardTemplate(product) {
+  if (product.FinalPrice < product.SuggestedRetailPrice) {
+    const discount = product.FinalPrice / product.SuggestedRetailPrice;
+    const discountPercent = Math.round((1 - discount) * 100);
+    return `<li class="product-card">
+            <a href="product_pages/?product=${product.Id}">
+                <img src="${product.Image}" alt="Image of ${product.Name}">
+                <h3 class="card__brand">${product.Brand.Name}</h2>
+                <h2 class="card__name">${product.NameWithoutBrand}</h3>
+                <p class="product-card_price">$${product.FinalPrice}    --    <span class="discount">${discountPercent}% Off</span></p>
+            </a>
+        </li>`;
+  }
   return `<li class="product-card">
             <a href="product_pages/?product=${product.Id}">
                 <img src="${product.Image}" alt="Image of ${product.Name}">
@@ -17,11 +29,13 @@ export default class ProductList {
     this.dataSource = dataSource;
     this.listElement = listElement;
   }
-  async init() { // initializes the product list by fetching data and rendering it
+  async init() {
+    // initializes the product list by fetching data and rendering it
     const list = await this.dataSource.getData();
     this.renderList(list);
   }
-  renderList(list) { // renders the list of products to the page
+  renderList(list) {
+    // renders the list of products to the page
     renderListWithTemplate(productCardTemplate, this.listElement, list);
   }
 }
