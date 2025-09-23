0fcd9eed210afa7513abc20858159d7b1bdd3b5c
velossoraptor <taliamolsen@gmail.com>
2025-09-23 11:50:20 -0400

    Added total to cart

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,6 +1,8 @@
 import { getLocalStorage, loadHeader, loadFooter } from "./utils.mjs";
 import { cartItemTemplate } from "./ShoppingCart.mjs";
 
+const cartFooter = document.querySelector(".cart-footer-hide");
+
 loadHeader();
 loadFooter();
 
@@ -8,6 +10,16 @@ function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
+  if (cartItems.length !== 0) {
+    let total = 0;
+    cartItems.map((item) => {
+      total += item.FinalPrice;
+    });
+    cartFooter.innerHTML = `Total: $${total}`;
+    cartFooter.classList.remove("cart-footer-hide");
+  } else {
+    cartFooter.classList.add("cart-footer-hide");
+  }
 }
 
 renderCartContents();
